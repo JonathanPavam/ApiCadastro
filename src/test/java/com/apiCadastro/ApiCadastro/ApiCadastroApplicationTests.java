8e683ccb2d494de944efd3afb381e9f25b418844
Jonathan Pavam <pavamjonathan@gmail.com>
2026-02-26 20:51:06 -0300

    Commit inicial

---
+++ src/test/java/com/apiCadastro/ApiCadastro/ApiCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiCadastro.ApiCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
