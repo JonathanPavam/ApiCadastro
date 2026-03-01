85eee11a40817eb3796b4a852788b6613ac3dde0
Jonathan Pavam <pavamjonathan@gmail.com>
2026-03-01 17:43:23 -0300

    fix: add unique in column email

--- src/main/java/com/apiCadastro/ApiCadastro/Pessoas/Controller/Service/PessoaModel.java
+++ src/main/java/com/apiCadastro/ApiCadastro/Pessoas/Controller/Service/PessoaModel.java
@@ -17,8 +17,12 @@ public class PessoaModel
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     @ManyToOne
