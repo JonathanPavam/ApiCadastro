297c8c1aa61cb4e9dc12f1233d4baf8a44d48576
Jonathan Pavam <pavamjonathan@gmail.com>
2026-03-04 21:21:40 -0300

    fix: Adicionando @GitIgnore para evitar looping de serialização ao listar os Json

--- src/main/java/com/apiCadastro/ApiCadastro/Tarefas/TarefasModel.java
+++ src/main/java/com/apiCadastro/ApiCadastro/Tarefas/TarefasModel.java
@@ -1,6 +1,7 @@
 package com.apiCadastro.ApiCadastro.Tarefas;
 
 import com.apiCadastro.ApiCadastro.Pessoas.PessoaModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,5 +23,6 @@ public class TarefasModel
     private String nome;
     private String dificuldade;
     @OneToMany(mappedBy = "tarefa")
+    @JsonIgnore
     private List<PessoaModel> pessoa;
 }
