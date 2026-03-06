6133f14f0611e88126e9d518f6ddfc205d02d9ad
Jonathan Pavam <pavamjonathan@gmail.com>
2026-03-05 22:09:49 -0300

    chore: DTO

--- src/main/java/com/apiCadastro/ApiCadastro/Pessoas/PessoaDto.java
+++ src/main/java/com/apiCadastro/ApiCadastro/Pessoas/PessoaDto.java
@@ -1,4 +1,23 @@
 package com.apiCadastro.ApiCadastro.Pessoas;
 
-public class PessoaDto {
+import com.apiCadastro.ApiCadastro.Tarefas.TarefasModel;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaDto
+{
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imagemUrl;
+    private String relevancia;
+
+    private TarefasModel tarefa;
+
 }
